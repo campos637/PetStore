ea7618cff960ae8c897901ed00b262d18d88524a
campos637 <tca637@gmail.com>
2022-03-01 12:42:40 -0300

    Finalizado o curso

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 public class Pet {
 
@@ -16,7 +17,7 @@ public class Pet {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
-    @Test
+    @Test(priority=1)
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
@@ -24,10 +25,73 @@ public class Pet {
                 .contentType("application/json")
                 .log().all()
                 .body(jsonBody)
-        .when()
+                .when()
                 .post(uri)
-        .then()
+                .then()
                 .log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body("name", is("Apollo"))
+                .body("status", is("available"))
+        ;
+    }
+
+    @Test(priority=2)
+    public void consultarPet(){
+        String petId = "19850813";
+
+        String token =
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                        .when()
+                        .get(uri + "/" + petId)
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("name", is("Apollo"))
+                        .body("category.name", is("AX2345LORT"))
+                        .body("status",is("available"))
+                        .extract()
+                        .path("category.name")
+                ;
+        System.out.println("O token é " + token);
+
+    }
+
+    @Test(priority=3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+                .when()
+                .put(uri)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Apollo"))
+                .body("status",is("sold"))
+        ;
+    }
+
+    @Test (priority = 4)
+    public void excluirPet(){
+        String petId = "19850813";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .when()
+                .delete(uri + "/" + petId)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is ("unknown"))
+                .body("message", is(petId))
+
+        ;
     }
 }
